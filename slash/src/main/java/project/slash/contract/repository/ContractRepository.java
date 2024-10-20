644f037d6d21c1f4c812eaf972419ce6232991e3
Yang-soeun <soeun8636@naver.com>
2024-10-20 13:20:31 +0900

    :sparkles: FEAT: 계약 테이블 관련 초기 구조 구현

---
+++ slash/src/main/java/project/slash/contract/repository/ContractRepository.java
@@ -0,0 +1,7 @@
+package project.slash.contract.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.slash.contract.model.Contract;
+
+public interface ContractRepository extends JpaRepository<Contract, Long> {
+}
