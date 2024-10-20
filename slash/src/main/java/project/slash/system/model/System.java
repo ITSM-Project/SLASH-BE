329d35a2f678be867d1cac458d5721bbd28e587d
Yang-soeun <soeun8636@naver.com>
2024-10-20 13:32:47 +0900

    :sparkles: FEAT: 시스템, 장비 테이블 관련 초기 구조 구현

---
+++ slash/src/main/java/project/slash/system/model/System.java
@@ -0,0 +1,15 @@
+package project.slash.system.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class System {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "system_id")
+    private Long id;
+    private String name;
+}
---
+++ slash/src/main/java/project/slash/system/repository/EquipmentRepository.java
@@ -0,0 +1,7 @@
+package project.slash.system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.slash.system.model.Equipment;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+}
