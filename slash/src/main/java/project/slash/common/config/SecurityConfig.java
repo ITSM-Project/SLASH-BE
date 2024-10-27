1795c33ac793fd1a0b0891143f69e23e20301223
ssoulistic <ssoulistic@gmail.com>
2024-10-28 00:05:55 +0900

    :recycle: REFACTOR: 코드 변경

--- slash/src/main/java/project/slash/common/config/SecurityConfig.java
+++ slash/src/main/java/project/slash/common/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
@@ -31,7 +32,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests((requests) -> requests
+			.authorizeHttpRequests(requests -> requests
 				.requestMatchers("/login", "/error").permitAll()
 				.requestMatchers("/contract-admin/**").hasRole("CONTRACT_ADMIN")
 				.requestMatchers("/service-admin/**").hasRole("SERVICE_ADMIN")
@@ -47,7 +48,7 @@ public class SecurityConfig {
 				.failureHandler(failureHandler)  // 로그인 실패 핸들러
 				.permitAll()
 			)
-			.logout((logout) -> logout
+			.logout(logout -> logout
 				.logoutUrl("/logout")
 				.logoutSuccessUrl("/login")
 				.invalidateHttpSession(true)  // 세션 무효화
@@ -56,12 +57,11 @@ public class SecurityConfig {
 			)
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)  // 필요 시 세션 생성
+				.sessionFixation(
+					SessionManagementConfigurer.SessionFixationConfigurer::changeSessionId)// 세션 ID 변경 전략 명시
 				.maximumSessions(1)  // 최대 허용 세션 개수 1개
 				.maxSessionsPreventsLogin(true)  // 기존 세션이 있으면 새로운 로그인 차단
 				.sessionRegistry(sessionRegistry())  // 세션 레지스트리 등록
-			)
-			.sessionManagement(session -> session
-				.sessionFixation().changeSessionId()  // 세션 ID 변경 전략 명시
 			);
 		return http.build();
 	}
