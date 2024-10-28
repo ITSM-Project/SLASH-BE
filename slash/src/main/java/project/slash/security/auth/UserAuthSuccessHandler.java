539ff6cc01046ba6ccd948ae304d6a04fc02182d
Yang-soeun <soeun8636@naver.com>
2024-10-28 21:15:53 +0900

    :tada: STYLE: 주석 제거

--- slash/src/main/java/project/slash/security/auth/UserAuthSuccessHandler.java
+++ slash/src/main/java/project/slash/security/auth/UserAuthSuccessHandler.java
@@ -1,45 +1,45 @@
-// package project.slash.security.auth;
-//
-// import java.io.IOException;
-// import java.util.Collection;
-//
-// import org.springframework.security.core.Authentication;
-// import org.springframework.security.core.GrantedAuthority;
-// import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-// import org.springframework.stereotype.Component;
-//
-// import jakarta.servlet.ServletException;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-//
-// @Component
-// public class UserAuthSuccessHandler implements AuthenticationSuccessHandler {
-//
-// 	@Override
-// 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-// 		Authentication authentication) throws IOException, ServletException {
-// 		// 사용자의 권한 정보 가져오기
-// 		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-//
-// 		// 역할에 따라 리다이렉트할 URL 결정
-// 		String redirectUrl = determineRedirectUrl(authorities);
-//
-// 		// 지정된 URL로 리다이렉트
-// 		response.sendRedirect(redirectUrl);
-// 	}
-//
-// 	private String determineRedirectUrl(Collection<? extends GrantedAuthority> authorities) {
-// 		for (GrantedAuthority authority : authorities) {
-// 			String role = authority.getAuthority();
-// 			if (role.equals("ROLE_USER")) {
-// 				return "/user/dashboard";
-// 			} else if (role.equals("ROLE_CONTRACT_ADMIN")) {
-// 				return "/contract-admin/dashboard";
-// 			} else if (role.equals("ROLE_SERVICE_ADMIN")) {
-// 				return "/service-admin/dashboard";
-// 			}
-// 		}
-// 		// 기본 리다이렉트 경로 (예: 권한 없는 사용자)
-// 		return "/";
-// 	}
-// }
+package project.slash.security.auth;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class UserAuthSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) throws IOException, ServletException {
+		// 사용자의 권한 정보 가져오기
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+		// 역할에 따라 리다이렉트할 URL 결정
+		String redirectUrl = determineRedirectUrl(authorities);
+
+		// 지정된 URL로 리다이렉트
+		response.sendRedirect(redirectUrl);
+	}
+
+	private String determineRedirectUrl(Collection<? extends GrantedAuthority> authorities) {
+		for (GrantedAuthority authority : authorities) {
+			String role = authority.getAuthority();
+			if (role.equals("ROLE_USER")) {
+				return "/user/dashboard";
+			} else if (role.equals("ROLE_CONTRACT_ADMIN")) {
+				return "/contract-admin/dashboard";
+			} else if (role.equals("ROLE_SERVICE_ADMIN")) {
+				return "/service-admin/dashboard";
+			}
+		}
+		// 기본 리다이렉트 경로 (예: 권한 없는 사용자)
+		return "/";
+	}
+}
--- slash/src/main/java/project/slash/user/service/UserService.java
+++ slash/src/main/java/project/slash/user/service/UserService.java
@@ -1,33 +1,33 @@
-// package project.slash.user.service;
-//
-// import java.util.List;
-//
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Service;
-//
-// import jakarta.annotation.PostConstruct;
-// import lombok.RequiredArgsConstructor;
-// import project.slash.user.model.User;
-// import project.slash.user.repository.UserRepository;
-//
-// @Service
-// @RequiredArgsConstructor
-// public class UserService {
-// 	private final UserRepository userRepository;
-// 	private final PasswordEncoder passwordEncoder;
-//
-// 	// 테스트용 데이터 자동 생성
-// 	@PostConstruct
-// 	public void initCustomsData() {
-// 		List<User> customs = List.of(
-// 			User.from("1", "ROLE_CONTRACT_ADMIN", "a", passwordEncoder.encode("1234"),
-// 				"test@test.com", "010-1111-1111"),
-// 			User.from("2", "ROLE_SERVICE_ADMIN", "b", passwordEncoder.encode("1234"),
-// 				"test@test.com", "010-1111-1111"),
-// 			User.from("3", "ROLE_USER", "c", passwordEncoder.encode("1234"),
-// 				"test@test.com", "010-1111-1111")
-// 		);
-//
-// 		userRepository.saveAll(customs);
-// 	}
-// }
+package project.slash.user.service;
+
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import project.slash.user.model.User;
+import project.slash.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	// 테스트용 데이터 자동 생성
+	@PostConstruct
+	public void initCustomsData() {
+		List<User> customs = List.of(
+			User.from("1", "ROLE_CONTRACT_ADMIN", "a", passwordEncoder.encode("1234"),
+				"test@test.com", "010-1111-1111"),
+			User.from("2", "ROLE_SERVICE_ADMIN", "b", passwordEncoder.encode("1234"),
+				"test@test.com", "010-1111-1111"),
+			User.from("3", "ROLE_USER", "c", passwordEncoder.encode("1234"),
+				"test@test.com", "010-1111-1111")
+		);
+
+		userRepository.saveAll(customs);
+	}
+}
