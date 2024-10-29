5893a0b737a4bae884699173c1e2eb61fe2a3703
hamin-yeong <mytokki0323@gmail.com>
2024-10-29 22:17:43 +0900

    [FEAT]: RequestManagerMain을 위한 controller,service구현

--- slash/src/main/java/project/slash/taskrequest/controller/TaskRequestController.java
+++ slash/src/main/java/project/slash/taskrequest/controller/TaskRequestController.java
@@ -1,34 +1,21 @@
 package project.slash.taskrequest.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import project.slash.common.response.BaseResponse;
 import project.slash.taskrequest.dto.request.TaskRequestDto;
-import project.slash.taskrequest.dto.response.TaskTypeDto;
+import project.slash.taskrequest.dto.response.RequestManagerMainResponseDto;
 import project.slash.taskrequest.service.TaskRequestService;
 
 @RestController
 @RequiredArgsConstructor
 public class TaskRequestController {
 	private final TaskRequestService taskRequestService;
-
-	/**
-	 * 현재 계약의 업무 유형 조회 메서드 입니다.
-	 *
-	 * @return 업무 유형
-	 */
-	@GetMapping("/all-task-types")
-	public BaseResponse<?> allTaskTypes() {
-		List<TaskTypeDto> allTaskTypes = taskRequestService.allTaskTypes();
-
-		return BaseResponse.ok(allTaskTypes);
-	}
 
 	/**
 	 * 요청 생성 메서드입니다.
@@ -41,5 +28,12 @@ public class TaskRequestController {
 		taskRequestService.createRequest(taskRequestDto);
 
 		return BaseResponse.ok();
+	}
+
+	@GetMapping("/monthly-data")
+	public BaseResponse<?> getMonthlyRequestData(@RequestParam("year") int year, @RequestParam("month") int month,
+		String user) {
+		RequestManagerMainResponseDto requestManager = taskRequestService.getMonthlyRequestData(year, month, "1");
+		return BaseResponse.ok(requestManager);
 	}
 }
