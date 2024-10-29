614c3a84af082c82cb9cd0b2d15753f7bcd0751f
ssoulistic <ssoulistic@gmail.com>
2024-10-29 23:47:11 +0900

    :sparkles: FEAT: CORS 설정 적용 및 jsonFilter로 교체, role name 변경

--- slash/src/main/java/project/slash/common/config/SecurityConfig.java
+++ slash/src/main/java/project/slash/common/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package project.slash.common.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
@@ -13,9 +16,16 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import project.slash.security.auth.CustomUserDetailService;
+import project.slash.security.auth.JsonAuthenticationFilter;
 import project.slash.security.auth.UserAuthFailureHandler;
 import project.slash.security.auth.UserAuthSuccessHandler;
 
@@ -23,30 +33,30 @@ import project.slash.security.auth.UserAuthSuccessHandler;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
-
+	private final ObjectMapper objectMapper;
 	private final CustomUserDetailService customUserDetailsService;
 	private final UserAuthSuccessHandler successHandler;
 	private final UserAuthFailureHandler failureHandler;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+		AuthenticationManager authenticationManager) throws Exception {
+		JsonAuthenticationFilter jsonFilter =
+			new JsonAuthenticationFilter("/login", objectMapper, customUserDetailsService, passwordEncoder());
+
+		jsonFilter.setAuthenticationManager(authenticationManager);
+		jsonFilter.setAuthenticationSuccessHandler(successHandler);
+		jsonFilter.setAuthenticationFailureHandler(failureHandler);
 		http
-			.csrf(csrf -> csrf.disable())
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(requests -> requests
-				.requestMatchers("/login", "/error").permitAll()
-				.requestMatchers("/contract-admin/**").hasRole("CONTRACT_ADMIN")
-				.requestMatchers("/service-admin/**").hasRole("SERVICE_ADMIN")
+				.requestMatchers("/login", "/logout", "/error").permitAll()
+				.requestMatchers("/requestManager/**").hasRole("REQUEST_MANAGER")
+				.requestMatchers("/contractManager/**").hasRole("CONTRACT_MANAGER")
 				.requestMatchers("/user/**").hasRole("USER")
 				.anyRequest().authenticated()
 			)
-			.formLogin(form -> form
-				.loginPage("/login") // 로그인 페이지 사용 - 추후 프론트 연결
-				.loginProcessingUrl("/login")
-				.usernameParameter("id")
-				.passwordParameter("password")
-				.successHandler(successHandler)  // 로그인 성공 핸들러
-				.failureHandler(failureHandler)  // 로그인 실패 핸들러
-				.permitAll()
+			.formLogin(AbstractHttpConfigurer::disable
 			)
 			.logout(logout -> logout
 				.logoutUrl("/logout")
@@ -62,8 +72,24 @@ public class SecurityConfig {
 				.maximumSessions(1)  // 최대 허용 세션 개수 1개
 				.maxSessionsPreventsLogin(true)  // 기존 세션이 있으면 새로운 로그인 차단
 				.sessionRegistry(sessionRegistry())  // 세션 레지스트리 등록
-			);
+			)
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.addFilterAt(jsonFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));  // 허용할 클라이언트 도메인
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 
 	@Bean
@@ -72,10 +98,9 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-		var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
-		builder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
-		return builder.build();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+		throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 
 	@Bean
