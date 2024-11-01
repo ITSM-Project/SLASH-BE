89d662d3402510f8e8bf2f0bade0aea186d449bc
ssoulistic <ssoulistic@gmail.com>
2024-11-01 12:48:52 +0900

    :sparkles: FEAT: 로그인 security 적용

--- slash/src/main/java/project/slash/user/controller/UserController.java
+++ slash/src/main/java/project/slash/user/controller/UserController.java
@@ -1,19 +1,38 @@
 package project.slash.user.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import project.slash.security.auth.dto.JwtTokenDTO;
+import project.slash.security.auth.service.AuthService;
+import project.slash.security.auth.dto.LoginRequestDTO;
+
+@RequiredArgsConstructor
 @RestController
 public class UserController {
+	private final AuthService authService;
 
-	@GetMapping("/contract-admin/dashboard")
-	public String contractAdminDashboard() {
-		return "contract-admin/dashboard";
+	@PostMapping("/login")
+	public ResponseEntity<JwtTokenDTO> login(@RequestBody LoginRequestDTO userLoginRequestDto) {
+		String userId = userLoginRequestDto.getId();
+		String password = userLoginRequestDto.getPassword();
+		JwtTokenDTO tokenInfo = authService.login(userId, password);
+		return ResponseEntity.status(HttpStatus.OK).body(tokenInfo);
 	}
 
-	@GetMapping("/service-admin/dashboard")
-	public String serviceAdminDashboard() {
-		return "service-admin/dashboard";
+	@GetMapping("contractManager/dashboard")
+	public String contractManagerDashboard() {
+		return "contractManager/dashboard";
+	}
+
+	@GetMapping("/serviceManager/dashboard")
+	public String serviceManagerDashboard() {
+		return "serviceManager/dashboard";
 	}
 
 	@GetMapping("/user/dashboard")
