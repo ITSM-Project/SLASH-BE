ff790a87c8be86e47848edf1a40d509cd7830382
seojung <6dreamhigh@naver.com>
2024-11-01 21:25:26 +0900

    ✨FEAT: 요청 관리 표에 대한 페이지네이션 처리를 위한 BaseResponse 함수 구현

--- slash/src/main/java/project/slash/common/response/BaseResponse.java
+++ slash/src/main/java/project/slash/common/response/BaseResponse.java
@@ -1,7 +1,12 @@
 package project.slash.common.response;
 
+import java.util.List;
+import java.util.Map;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import project.slash.taskrequest.dto.request.TaskResponseRequestDTO;
 
 @JsonPropertyOrder({"success", "data", "message"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -17,5 +22,17 @@ public record BaseResponse<T>(Boolean success, T data, String message) {
 
 	public static BaseResponse<Void> error(String message) {
 		return new BaseResponse<>(false, null, message);
+	}
+
+	public static <T> BaseResponse<Map<String, Object>> ok(Map<String, Object> responseData) {
+		return new BaseResponse<>(true, responseData, null);
+	}
+
+	public static BaseResponse<Map<String, Object>> ok(Object data, int totalPages) {
+		Map<String, Object> responseData = Map.of(
+			"results", data,
+			"totalPages", totalPages
+		);
+		return new BaseResponse<>(true, responseData, null);
 	}
 }
