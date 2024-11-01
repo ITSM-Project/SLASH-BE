4ff06c2aaa4886f038f2630ea43f9c851e1e4290
seojung <6dreamhigh@naver.com>
2024-11-01 21:24:40 +0900

    ✨FEAT: 요청 관리 표에 대한 컨트롤러 클래스 구현

--- slash/src/main/java/project/slash/taskrequest/controller/TaskRequestController.java
+++ slash/src/main/java/project/slash/taskrequest/controller/TaskRequestController.java
@@ -1,16 +1,23 @@
 package project.slash.taskrequest.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import project.slash.common.response.BaseResponse;
 import project.slash.taskrequest.dto.request.TaskRequestDto;
+import project.slash.taskrequest.dto.request.TaskResponseRequestDTO;
 import project.slash.taskrequest.dto.response.AllTaskTypeDto;
+import project.slash.taskrequest.model.constant.RequestStatus;
 import project.slash.taskrequest.service.TaskRequestService;
 
 @RestController
@@ -42,4 +49,32 @@ public class TaskRequestController {
 
 		return BaseResponse.ok();
 	}
+
+	@GetMapping("/requests")
+	public BaseResponse<?> readRequest(
+		@RequestParam(required = false) String equipmentName,
+		@RequestParam(required = false) String type,
+		@RequestParam(required = false) String taskDetail,
+		@RequestParam(required = false) RequestStatus status,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "4") int size
+	) {
+		// Pageable 객체 생성
+		Pageable pageable = PageRequest.of(page - 1, size);
+
+		// Page 객체 반환
+		Page<TaskResponseRequestDTO> taskResponseRequestDTOS =
+			taskRequestService.findFilteredRequests(equipmentName, type, taskDetail, status, pageable);
+
+		// 응답 데이터로 페이지네이션 정보를 포함하여 반환
+		Map<String, Object> responseData = Map.of(
+			"results", taskResponseRequestDTOS.getContent(),
+			"totalPages", taskResponseRequestDTOS.getTotalPages(),
+			"currentPage", taskResponseRequestDTOS.getNumber() + 1, // 현재 페이지
+			"totalItems", taskResponseRequestDTOS.getTotalElements() // 총 항목 수
+		);
+
+		return BaseResponse.ok(responseData);
+	}
+
 }
