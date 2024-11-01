bf459ae9e4276f6a979a4b216140a86844609c2d
seojung <6dreamhigh@naver.com>
2024-11-01 21:17:37 +0900

    ✨FEAT: 요청에 대한 표를 위한 DTO 생성

---
+++ slash/src/main/java/project/slash/taskrequest/dto/request/TaskResponseRequestDTO.java
@@ -0,0 +1,27 @@
+package project.slash.taskrequest.dto.request;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.slash.taskrequest.model.constant.RequestStatus;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskResponseRequestDTO {
+	private String title;
+	private String content;
+	private Boolean dueOnTime;
+	private RequestStatus status;
+	private String equipmentName;
+	private String type;
+	private String taskDetail;
+	private String requesterName;
+	private String managerName;
+	private LocalDateTime createTime;
+	private LocalDateTime updateTime;
+
+}
+
