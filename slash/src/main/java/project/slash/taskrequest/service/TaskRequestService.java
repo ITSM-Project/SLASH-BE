75fc52709098007e7ae95f4b48249db95a77276c
seojung <6dreamhigh@naver.com>
2024-11-01 21:24:29 +0900

    ✨FEAT: 요청 관리 표에 대한 서비스 클래스 구현

--- slash/src/main/java/project/slash/taskrequest/service/TaskRequestService.java
+++ slash/src/main/java/project/slash/taskrequest/service/TaskRequestService.java
@@ -5,6 +5,8 @@ import static project.slash.taskrequest.exception.TaskRequestErrorCode.*;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +15,11 @@ import project.slash.common.exception.BusinessException;
 import project.slash.system.model.Equipment;
 import project.slash.system.repository.EquipmentRepository;
 import project.slash.taskrequest.dto.request.TaskRequestDto;
+import project.slash.taskrequest.dto.request.TaskResponseRequestDTO;
 import project.slash.taskrequest.dto.response.AllTaskTypeDto;
 import project.slash.taskrequest.model.TaskRequest;
 import project.slash.taskrequest.model.TaskType;
+import project.slash.taskrequest.model.constant.RequestStatus;
 import project.slash.taskrequest.repository.TaskRequestRepository;
 import project.slash.taskrequest.repository.TaskTypeRepository;
 
@@ -33,7 +37,8 @@ public class TaskRequestService {
 
 		Equipment equipment = findEquipment(taskRequestDto);
 
-		TaskRequest taskRequest = TaskRequest.from(taskRequestDto, taskType, null, equipment); //TODO: 유저는 로그인 기능 완료 후 넣기
+		TaskRequest taskRequest = TaskRequest.from(taskRequestDto, taskType, null,
+			equipment); //TODO: 유저는 로그인 기능 완료 후 넣기
 
 		taskRequestRepository.save(taskRequest);
 	}
@@ -51,5 +56,12 @@ public class TaskRequestService {
 		return taskTypeRepository.findTaskTypeByTaskRequestInfo(taskRequestDto.getType(),
 			taskRequestDto.getTaskDetail(),
 			taskRequestDto.isServiceRelevance()).orElseThrow(() -> new BusinessException(NOT_FOUND_TASK_TYPE));
+	}
+
+
+	public Page<TaskResponseRequestDTO> findFilteredRequests(String equipmentName, String type, String taskDetail, RequestStatus status
+		,Pageable pageable
+		) {
+		return taskRequestRepository.findFilteredRequests(equipmentName, type, taskDetail, status, pageable);
 	}
 }
