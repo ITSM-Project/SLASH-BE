1d27f7d4756d276b25a0bd3fdd79b89f5b974dd2
hamin-yeong <mytokki0323@gmail.com>
2024-11-02 20:04:18 +0900

    :sparkles: FEAT: 통계 조회 기능 구현

--- slash/src/main/java/project/slash/statistics/repository/StatisticsRepository.java
+++ slash/src/main/java/project/slash/statistics/repository/StatisticsRepository.java
@@ -1,8 +1,13 @@
 package project.slash.statistics.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import project.slash.statistics.dto.StatsDto;
 import project.slash.statistics.model.Statistics;
 
 public interface StatisticsRepository extends JpaRepository<Statistics, Long>, StatisticsRepositoryCustom {
+	List<StatsDto> getStatisticsByServiceTypeAndPeriodAndTargetSystem(String serviceType, String period,
+		String targetSystem);
 }
--- slash/src/main/java/project/slash/statistics/repository/StatisticsRepositoryCustomImpl.java
+++ slash/src/main/java/project/slash/statistics/repository/StatisticsRepositoryCustomImpl.java
@@ -11,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.Projections;
@@ -20,12 +21,16 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 import project.slash.statistics.dto.MonthlyDataDto;
+import project.slash.statistics.dto.MonthlyServiceStatsDto;
 
 @Repository
 @RequiredArgsConstructor
 public class StatisticsRepositoryCustomImpl implements StatisticsRepositoryCustom {
 	@Autowired
 	private final JPAQueryFactory queryFactory;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public List<MonthlyDataDto> getMonthlyData() {
@@ -42,15 +47,43 @@ public class StatisticsRepositoryCustomImpl implements StatisticsRepositoryCusto
 				systems.name,
 				taskRequest.count(),
 				systemIncident.incidentTime.sum(),
-				Expressions.numberTemplate(Integer.class, "DAY(LAST_DAY({0}))", taskRequest.createTime))) // LAST_DAY 사용
+				Expressions.numberTemplate(Integer.class, "DAY(LAST_DAY({0}))", taskRequest.createTime),
+				systemIncident.count()
+			))
 			.from(taskRequest)
 			.leftJoin(equipment).on(taskRequest.equipment.id.eq(equipment.id))
 			.leftJoin(systems).on(equipment.systems.id.eq(systems.id))
 			.leftJoin(systemIncident).on(systemIncident.taskRequest.id.eq(taskRequest.id))
 			.leftJoin(taskType).on(taskRequest.taskType.id.eq(taskType.id))// 현재 년-월보다 이전 것만 필터링
 			.where(yearMonth.eq(previousMonth))
-			.groupBy(yearMonth, systems.name)
-			.orderBy(yearMonth.asc())
+			.groupBy(systems.name)
+			.orderBy(systems.name.asc())
 			.fetch();
 	}
+
+	@Override
+	public void saveMonthlyData(List<MonthlyServiceStatsDto> statsDtoList) {
+		String sql = "INSERT INTO statistics (`date`, service_type, grade, score, period, weighted_score, " +
+			"approval_status, total_downtime, request_count, evaluation_item_id, target_system, estimate,system_incident_count,due_on_time_count) "
+			+
+			"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+
+		jdbcTemplate.batchUpdate(sql, statsDtoList, 50, (ps, dto) -> {
+			ps.setDate(1, java.sql.Date.valueOf(dto.getDate()));
+			ps.setString(2, dto.getServiceType());
+			ps.setString(3, dto.getGrade());
+			ps.setDouble(4, dto.getScore());
+			ps.setString(5, dto.getPeriod());
+			ps.setDouble(6, dto.getWeightedScore());
+			ps.setBoolean(7, dto.isApprovalStatus());
+			ps.setLong(8, dto.getTotalDowntime());
+			ps.setLong(9, dto.getRequestCount());
+			ps.setLong(10, dto.getEvaluationItemId());
+			ps.setString(11, dto.getTargetSystem());
+			ps.setDouble(12, dto.getEstimate());
+			ps.setLong(13, dto.getSystemIncidentCount());
+			ps.setLong(14, dto.getDueOnTimeCount());
+		});
+	}
+
 }
