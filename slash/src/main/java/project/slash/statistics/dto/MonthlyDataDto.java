c5f64496a3f326aac486ff41f1e988c1efc533dc
hamin-yeong <mytokki0323@gmail.com>
2024-11-02 20:03:00 +0900

    :sparkles: FEAT: 월간 장비별 통계 데이터 systemIncidentCount 추가 및 toString함수 제거

--- slash/src/main/java/project/slash/statistics/dto/MonthlyDataDto.java
+++ slash/src/main/java/project/slash/statistics/dto/MonthlyDataDto.java
@@ -1,28 +1,15 @@
 package project.slash.statistics.dto;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class MonthlyDataDto {
-	@DateTimeFormat(pattern = "yyyy-MM")
 	private String yearMonth;
 	private String systemName;
 	private long requestCount;
 	private long totalDownTime;
 	private int lastDay;
-
-	@Override
-	public String toString() {
-		return "MonthlyDataDto{" +
-			"yearMonth=" + yearMonth +
-			", systemName='" + systemName + '\'' +
-			", requestCount=" + requestCount +
-			", totalDownTime=" + totalDownTime +
-			", lastDay=" + lastDay +
-			'}';
-	}
+	private long systemIncidentCount;
 }
