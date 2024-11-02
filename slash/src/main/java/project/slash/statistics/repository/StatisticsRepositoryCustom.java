546470d99c71e29fe1573cf3522af028ffa1405d
hamin-yeong <mytokki0323@gmail.com>
2024-11-02 20:06:56 +0900

    :sparkles: FEAT: 통계 자동 계산 기능 구현 및 자동계산 데이터 삽입 메서드 선언

--- slash/src/main/java/project/slash/statistics/repository/StatisticsRepositoryCustom.java
+++ slash/src/main/java/project/slash/statistics/repository/StatisticsRepositoryCustom.java
@@ -3,7 +3,10 @@ package project.slash.statistics.repository;
 import java.util.List;
 
 import project.slash.statistics.dto.MonthlyDataDto;
+import project.slash.statistics.dto.MonthlyServiceStatsDto;
 
 public interface StatisticsRepositoryCustom {
 	List<MonthlyDataDto> getMonthlyData();
+
+	void saveMonthlyData(List<MonthlyServiceStatsDto> monthlyServiceStatsDtoList);
 }
