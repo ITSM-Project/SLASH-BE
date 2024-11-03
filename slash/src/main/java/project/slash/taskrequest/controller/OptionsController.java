076d2321620d4355e18e9ca58dab1f2bc00c9ebb
seojung <6dreamhigh@naver.com>
2024-11-04 03:06:12 +0900

    :sparkles: FEAT: OptionsController 주석 추가

--- slash/src/main/java/project/slash/taskrequest/controller/OptionsController.java
+++ slash/src/main/java/project/slash/taskrequest/controller/OptionsController.java
@@ -16,16 +16,31 @@ public class OptionsController {
 
 	private final OptionsService optionsService;
 
+	/**
+	 * 시스템의 고유한 이름 목록을 조회하는 메서드입니다.
+	 *
+	 * @return 시스템 이름 목록
+	 */
 	@GetMapping("/systems")
 	public List<String> getDistinctSystemNames() {
 		return optionsService.getDistinctSystemNames();
 	}
 
+	/**
+	 * 현재 계약의 업무 유형 목록을 조회하는 메서드입니다.
+	 *
+	 * @return 업무 유형 목록
+	 */
 	@GetMapping("/task-type")
 	public List<String> getDistinctTaskType() {
 		return optionsService.getDistinctTaskTypes();
 	}
 
+	/**
+	 * 현재 계약의 업무 세부 사항 목록을 조회하는 메서드입니다.
+	 *
+	 * @return 업무 세부 사항 목록
+	 */
 	@GetMapping("/task-detail")
 	public List<String> getDistinctTaskDetails() {
 		return optionsService.getDistinctTaskDetails();
