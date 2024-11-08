76e872049cf3fd55e515716ca34e5b6f244b4078
ssoulistic <ssoulistic@gmail.com>
2024-11-08 22:40:34 +0900

    :sparkles: FEAT: 공통 입력 Request DTO 설정

---
+++ slash/src/main/java/project/slash/statistics/dto/request/RequestStatisticsDto.java
@@ -0,0 +1,11 @@
+package project.slash.statistics.dto.request;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+
+@Getter
+public class RequestStatisticsDto {
+	private Long evaluationItemId;
+	private LocalDate endDate;
+}
