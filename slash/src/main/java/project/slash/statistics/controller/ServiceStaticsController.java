59056e205e667d5628c800d5a3a9fec0890c7d6c
김수지 <tvfxq0214@gmail.com>
2024-11-08 11:08:17 +0900

    ✨ FEAT: 서비스요청 통계 컨트롤러 및 리퀘스트Dto 생성

---
+++ slash/src/main/java/project/slash/statistics/controller/ServiceStaticsController.java
@@ -0,0 +1,29 @@
+package project.slash.statistics.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import project.slash.common.response.BaseResponse;
+import project.slash.statistics.dto.request.RequestStatisticsDto;
+import project.slash.statistics.service.StatisticsService;
+
+@RestController
+@RequiredArgsConstructor
+public class ServiceStaticsController {
+
+	private final StatisticsService statisticsService;
+
+	/**
+	 * @param requestStatisticsDto 통계낼 evaluationId와 Date 요청정보
+	 * @return 저장성공여부
+	 */
+	@PostMapping("/contract-manager/service-statistic")
+	public BaseResponse<Void> getServiceStatics(
+		@RequestBody @Valid RequestStatisticsDto requestStatisticsDto) {
+		statisticsService.getServiceTaskStatics(requestStatisticsDto);
+		return BaseResponse.ok();
+	}
+}
---
+++ slash/src/main/java/project/slash/statistics/dto/request/RequestStatisticsDto.java
@@ -0,0 +1,10 @@
+package project.slash.statistics.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class RequestStatisticsDto {
+
+	private Long evaluationItemId;
+	private String endDate;
+}
