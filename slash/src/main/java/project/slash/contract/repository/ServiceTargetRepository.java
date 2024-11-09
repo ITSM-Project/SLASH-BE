a10c6be500ac9e75f4a85f9c1863a4506129f735
ssoulistic <ssoulistic@gmail.com>
2024-11-10 01:39:25 +0900

    :sparkles: FEAT: 장애 적기처리율 서비스 로직 구현

--- slash/src/main/java/project/slash/contract/repository/ServiceTargetRepository.java
+++ slash/src/main/java/project/slash/contract/repository/ServiceTargetRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.slash.contract.model.ServiceTarget;
 
 public interface ServiceTargetRepository extends JpaRepository<ServiceTarget, Long> {
+	List<ServiceTarget> getServiceTargetByEvaluationItem_Id(Long evaluationItemId);
 }
