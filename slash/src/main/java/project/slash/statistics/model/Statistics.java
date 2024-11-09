3c55b0e4a3eb808e1fe3135ed391ae83fc0063ed
ssoulistic <ssoulistic@gmail.com>
2024-11-10 01:31:36 +0900

    :sparkles: FEAT: 정적 팩토리 메서드 추가

--- slash/src/main/java/project/slash/statistics/model/Statistics.java
+++ slash/src/main/java/project/slash/statistics/model/Statistics.java
@@ -8,11 +8,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import project.slash.contract.model.EvaluationItem;
+import project.slash.statistics.dto.IncidentInfoDto;
 
 import java.time.LocalDate;
 
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 public class Statistics {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,7 +59,31 @@ public class Statistics {
 	@Column(name = "system_incident_count")
 	private long systemIncidentCount;
 
+	private Boolean isAuto;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "evaluation_item_id")
 	private EvaluationItem evaluationItems;
+
+	public static Statistics fromIncidentTask(IncidentInfoDto incidentInfoDto, LocalDate date, double score,
+		double weightedScore, String grade, double estimate, EvaluationItem evaluationItem) {
+		return Statistics.builder()
+			.targetSystem("전체")
+			.targetEquipment("전체")
+			.serviceType("장애 적기처리율")
+			.date(date)
+			.approvalStatus(false)
+			.grade(grade)
+			.score(score)
+			.period("월별")
+			.totalDowntime(0)
+			.weightedScore(weightedScore)
+			.requestCount(incidentInfoDto.getTotalIncidentCount())
+			.systemIncidentCount(incidentInfoDto.getTotalIncidentCount())
+			.dueOnTimeCount(incidentInfoDto.getTotalIncidentCount() - incidentInfoDto.getTotalOverdueCount())
+			.estimate(estimate)
+			.evaluationItems(evaluationItem)
+			.isAuto(false)
+			.build();
+	}
 }
