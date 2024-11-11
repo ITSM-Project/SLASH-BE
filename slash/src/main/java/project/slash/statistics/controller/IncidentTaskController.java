83473cd6cb2bb95840d01b80bc8b0be4df6b235d
ssoulistic <ssoulistic@gmail.com>
2024-11-11 11:28:18 +0900

    :recycle: REFACTOR: javdoc 추가, API 위치 변경

--- slash/src/main/java/project/slash/statistics/controller/IncidentTaskController.java
+++ slash/src/main/java/project/slash/statistics/controller/IncidentTaskController.java
@@ -8,17 +8,22 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import project.slash.common.response.BaseResponse;
 import project.slash.statistics.dto.request.RequestStatisticsDto;
-import project.slash.statistics.service.IncidentStatisticsService;
+import project.slash.statistics.service.StatisticsService;
 
 @RestController
 @RequiredArgsConstructor
 public class IncidentTaskController {
-	private final IncidentStatisticsService incidentStatisticsService;
+	private final StatisticsService statisticsService;
 
-	@PostMapping("/common/statistics/incident-statistics")
+	/**
+	 * 지정된 날의 해당 월의 1일부터 지정된 날까지 장애 적기 처리율을 저장하는 메서드 입니다.
+	 *
+	 * @return success:true
+	 */
+	@PostMapping("/common/incident-statistics")
 	public BaseResponse<Void> addIncidentDueOnTimeRate(
 		@RequestBody @Valid RequestStatisticsDto requestStatisticsDto) {
-		incidentStatisticsService.getIncidentStatistics(requestStatisticsDto);
+		statisticsService.getIncidentStatistics(requestStatisticsDto);
 		return BaseResponse.ok();
 	}
 }
