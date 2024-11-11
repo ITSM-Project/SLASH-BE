73192ac0c9d6526b36071277f710c9e8bb144c22
Yang-soeun <soeun8636@naver.com>
2024-11-11 16:56:14 +0900

    :fire: REMOVE: 사용 안하는 import 문 제거

--- slash/src/main/java/project/slash/statistics/repository/StatisticsRepository.java
+++ slash/src/main/java/project/slash/statistics/repository/StatisticsRepository.java
@@ -1,13 +1,10 @@
 package project.slash.statistics.repository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import project.slash.statistics.model.Statistics;
 
