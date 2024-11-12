17033b7e78fca732e3ebda4a049430ed721d6160
Yang-soeun <soeun8636@naver.com>
2024-11-13 05:21:24 +0900

    :sparkles: FEAT: 통계 삭제 기능 구현

--- slash/src/main/java/project/slash/statistics/service/StatisticsService.java
+++ slash/src/main/java/project/slash/statistics/service/StatisticsService.java
@@ -128,4 +128,12 @@ public class StatisticsService {
 
 		return statisticsMapper.toCalculatedStatisticsDtos(statistics);
 	}
+
+	@Transactional
+	public void deleteStatistics(Long evaluationItemId, LocalDate calculateTime) {
+		List<Statistics> statistics = statisticsRepository.findByEvaluationItemIdAndCalculateTime(
+			evaluationItemId, calculateTime);
+
+		statisticsRepository.deleteAll(statistics);
+	}
 }
