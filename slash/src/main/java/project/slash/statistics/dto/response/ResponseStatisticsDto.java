668dd7e9e0d683f33904c1495318b86c28887688
nyeongha <49603260+nyeongha@users.noreply.github.com>
2024-11-17 19:11:47 +0900

    [REFACTOR]: 서비스 가동률 반환타입 ResponseStatisticsDto 로 통일

--- slash/src/main/java/project/slash/statistics/dto/response/ResponseStatisticsDto.java
+++ slash/src/main/java/project/slash/statistics/dto/response/ResponseStatisticsDto.java
@@ -6,14 +6,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.slash.contract.dto.response.ContractDataDto;
 import project.slash.statistics.dto.IncidentInfoDto;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ResponseStatisticsDto {
 	private String targetSystem;
@@ -30,6 +33,8 @@ public class ResponseStatisticsDto {
 	private long systemIncidentCount;
 	private Long evaluationItemId;
 	private LocalDate date;
+	private boolean approvalStatus;
+	private boolean isAuto;
 
 	public static ResponseStatisticsDto fromResponseServiceTask(ResponseServiceTaskDto responseServiceTaskDto,
 		double score, double weightedScore, String grade, LocalDate date) {
@@ -71,4 +76,48 @@ public class ResponseStatisticsDto {
 			.evaluationItemId(evaluationItemId)
 			.build();
 	}
+
+	public static ResponseStatisticsDto of(ContractDataDto contractDataDto,MonthlyDataDto monthlyDataDto,EvaluatedDto evaluatedDto,LocalDate date) {
+		return ResponseStatisticsDto.builder()
+			.date(date)
+			.serviceType(contractDataDto.getCategory())
+			.targetEquipment(monthlyDataDto.getEquipmentName())
+			.grade(evaluatedDto.getGrade())
+			.score(evaluatedDto.getScore())
+			.period("월별")
+			.weightedScore(evaluatedDto.getWeightedScore())
+			.approvalStatus(false)
+			.totalDowntime(monthlyDataDto.getTotalDownTime())
+			.requestCount(monthlyDataDto.getRequestCount())
+			.evaluationItemId(evaluatedDto.getEvaluationItemId())
+			.targetSystem(monthlyDataDto.getSystemName())
+			.estimate(evaluatedDto.getScore())
+			.systemIncidentCount(monthlyDataDto.getSystemIncidentCount())
+			.dueOnTimeCount(-1)
+			.isAuto(true)
+			.build();
+	}
+
+	public static ResponseStatisticsDto ofResponseStatisticsDto(EvaluatedDto evaluatedDto,ResponseStatisticsDto responseStatisticsDto,double averageScore,double averageWeightedScore,long totalDowntime,long totalRequestCount,long evaluationItemId,long totalSystemIncidentCount) {
+		return ResponseStatisticsDto.builder()
+			.date(responseStatisticsDto.getDate())
+			.serviceType(responseStatisticsDto.getServiceType())
+			.targetEquipment("전체")
+			.grade(evaluatedDto.getGrade())
+			.score(averageScore)
+			.period("월별")
+			.weightedScore(averageWeightedScore)
+			.approvalStatus(false)
+			.totalDowntime(totalDowntime)
+			.requestCount(totalRequestCount)
+			.evaluationItemId(evaluationItemId)
+			.targetSystem("전체")
+			.estimate(averageScore)
+			.systemIncidentCount(totalSystemIncidentCount)
+			.dueOnTimeCount(-1)
+			.isAuto(true)
+			.build();
+	}
+
+
 }
